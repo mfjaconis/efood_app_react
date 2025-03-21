ee8685cceb3b77c014051232938c564b70d1c21b
mfjaconis <matheusjaconis@gmail.com>
2025-03-21 17:13:05 -0300

    bug

--- src/components/ListRestaurants/index.jsx
+++ src/components/ListRestaurants/index.jsx
@@ -1,54 +1,53 @@
 import CardRestaurants from "./../CardRestaurants/index";
 import { List, ListContainer } from "./styles";
 
-function ListProducts() {
+function ListRestaurants() {
 	return (
-		<>
-			<ListContainer>
-				<div className="container">
-					<List>
-						<CardRestaurants
-							title="Hioki Sushi "
-							categories="Destaque da semana"
-							categories="Japonesa"
-							rating="4.9"
-							description="Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!"
-						/>
-						<CardRestaurants
-							title="La Dolce Vita Trattoria "
-							categories="Italiana"
-							rating="4.9"
-							description="Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!"
-						/>
-						<CardRestaurants
-							title="La Dolce Vita Trattoria"
-							categories="Italiana"
-							rating="4.9"
-							description="Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!"
-						/>
-						<CardRestaurants
-							title="La Dolce Vita Trattoria "
-							categories="Italiana"
-							rating="4.9"
-							description="Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!"
-						/>
-						<CardRestaurants
-							title="La Dolce Vita Trattoria "
-							categories="Italiana"
-							rating="4.9"
-							description="Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!"
-						/>
-						<CardRestaurants
-							title="La Dolce Vita Trattoria "
-							categories="Italiana"
-							rating="4.9"
-							description="Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!"
-						/>
-					</List>
-				</div>
-			</ListContainer>
-		</>
+		<ListContainer>
+			<div className="container">
+				<List>
+					<CardRestaurants
+						title="Hioki Sushi "
+						// biome-ignore lint/suspicious/noDuplicateJsxProps: <explanation>
+						categories="Destaque da semana"
+						categories="Japonesa"
+						rating="4.9"
+						description="Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida.Experimente o Japão sem sair do lar com nosso delivery!"
+					/>
+					<CardRestaurants
+						title="La Dolce Vita Trattoria "
+						categories="Italiana"
+						rating="4.9"
+						description="Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida.Experimente o Japão sem sair do lar com nosso delivery!"
+					/>
+					<CardRestaurants
+						title="La Dolce Vita Trattoria"
+						categories="Italiana"
+						rating="4.9"
+						description="Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida.Experimente o Japão sem sair do lar com nosso delivery!"
+					/>
+					<CardRestaurants
+						title="La Dolce Vita Trattoria "
+						categories="Italiana"
+						rating="4.9"
+						description="Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida.Experimente o Japão sem sair do lar com nosso delivery!"
+					/>
+					<CardRestaurants
+						title="La Dolce Vita Trattoria "
+						categories="Italiana"
+						rating="4.9"
+						description="Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida.Experimente o Japão sem sair do lar com nosso delivery!"
+					/>
+					<CardRestaurants
+						title="La Dolce Vita Trattoria "
+						categories="Italiana"
+						rating="4.9"
+						description="Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida.Experimente o Japão sem sair do lar com nosso delivery!"
+					/>
+				</List>
+			</div>
+		</ListContainer>
 	);
 }
 
-export default ListProducts;
+export default ListRestaurants;
