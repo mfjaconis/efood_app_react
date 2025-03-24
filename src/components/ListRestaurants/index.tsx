c4248138656fa73f88f365dfea4b7711e17dd936
mfjaconis <matheusjaconis@gmail.com>
2025-03-24 15:12:30 -0300

    build

--- src/components/ListRestaurants/index.tsx
+++ src/components/ListRestaurants/index.tsx
@@ -35,7 +35,7 @@ function ListRestaurants() {
 						title="La Dolce Vita Trattoria "
 						categories={["Italiana"]}
 						rating="4.9"
-						description="Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida.Experimente o Japão sem sair do lar com nosso delivery!"
+						description="Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida.Experimente o Japão sem sair do lar com nosso delivery!."
 					/>
 					<CardRestaurants
 						title="La Dolce Vita Trattoria "
