45a095c153e610611d2f0d3e20e6dcfac8491004
mfjaconis <matheusjaconis@gmail.com>
2025-03-29 12:49:13 -0300

    build

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import api from "../services/api";
-import cartReducer from "./Reducers/cart";
+import cartReducer from "./reducers/cart";
 
 export const store = configureStore({
 	reducer: {
