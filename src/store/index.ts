0c22015dd33e8cd4a7cfced56a2b1b6f2ab98e92
Matheus Fernandes Jaconis <115511423+mfjaconis@users.noreply.github.com>
2025-03-29 12:43:51 -0300

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import api from "../services/api";
-import cartReducer from "./reducers/cart";
+import cartReducer from "./Reducers/cart";
 
 export const store = configureStore({
 	reducer: {
